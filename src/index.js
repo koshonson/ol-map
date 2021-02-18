1240809d3bc7dfe29e11e22ab1a2080923fc4df6
michaelKosatka <michael.kosatka@seznam.cz>
2021-02-18 19:50:30 +0100

    basic OL map view in Krovak projection

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-console.log('Hello world, from index.js');
-console.log('Hello world, from index.js');
+import 'ol/ol.css';
+import './krovakProj';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+	target: 'map',
+	layers: [
+		new TileLayer({
+			source: new OSM()
+		})
+	],
+	view: new View({
+		projection: 'EPSG:5514',
+		center: [-749992.3, -1045788.0],
+		extent: [-925000, -1444353.536, -400646.464, -920000],
+		zoom: 10,
+		minZoom: 2
+	})
+});
