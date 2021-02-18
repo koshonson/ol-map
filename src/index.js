333c324613a6c64f875515087f3c8e694b3d7e03
michaelKosatka <michael.kosatka@seznam.cz>
2021-02-18 20:13:01 +0100

    added CUZK WMS TileLayer GR_ZM25 to the map

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import 'ol/ol.css';
 import './krovakProj';
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import TileWMS from 'ol/source/TileWMS';
 
 const map = new Map({
 	target: 'map',
 	layers: [
 		new TileLayer({
-			source: new OSM()
+			extent: [-905000, -1230000, -430000, -934999],
+			source: new TileWMS({
+				url: 'https://geoportal.cuzk.cz/WMS_ZM25_PUB/WMService.aspx',
+				params: { LAYERS: 'GR_ZM25', TILED: true, FORMAT: 'image/png' }
+			})
 		})
 	],
 	view: new View({
