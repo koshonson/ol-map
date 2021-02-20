94c39ad84cd32bc79dba063272071e950352371b
michaelKosatka <michael.kosatka@seznam.cz>
2021-02-20 23:32:32 +0100

    moved to a separate dir for a more consistent map dir structure

---
+++ src/map/views/index.js
@@ -0,0 +1,10 @@
+import { View } from 'ol';
+import { krovakDef } from '../projections/projectionsDef';
+
+export const luzinyView = new View({
+	projection: 'EPSG:5514',
+	extent: krovakDef.extent,
+	center: [-749992.3, -1045788.0],
+	zoom: 10,
+	minZoom: 3
+});
